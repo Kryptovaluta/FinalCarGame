c8dd6a9215e76246d49446de1fd33aa066e4dad3
Keon Brown <brownkeon4@gmail.com>
2021-02-16 20:43:00 -0500

    MAde comment

---
+++ Refresher.py
@@ -0,0 +1,34 @@
+command = ''
+started = False
+while True:
+    command = input('> ').lower()
+    if command == 'start':
+        if started:
+            print('Car is already on!')
+        else:
+            started = True
+            print('Car started,lets go!')
+    elif command == 'stop':
+        if not started:
+            print("Car is already stopped!")
+        else:
+            started = False
+            print('Car stopped.')
+    elif command == 'help':
+        print("""
+    start - to start the car
+    stop - to stop the car
+    quit - to quit
+        """)
+    elif command == 'quit':
+        print('Goodbye!')
+        break
+    else:
+        print("Sorry I don't understand")
+
+#CARGAME
+
+
+
+
+
